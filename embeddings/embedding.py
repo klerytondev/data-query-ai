3714d8a2118443d011faa244b2b2de83cae95693
klerytondev <kleryton.dev@gmail.com>
2025-03-07 20:54:09 -0300

    Atualizar importação do Chroma para o módulo correto na implementação de embeddings

--- embeddings/embedding.py
+++ embeddings/embedding.py
@@ -1,6 +1,7 @@
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain_community.document_loaders import CSVLoader
 from langchain_openai import OpenAIEmbeddings
 
