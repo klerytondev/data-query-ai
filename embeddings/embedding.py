203155bdcddf8d53b5954e5441d6a726802436ae
klerytondev <kleryton.dev@gmail.com>
2025-03-07 22:05:47 -0300

    fix

--- embeddings/embedding.py
+++ embeddings/embedding.py
@@ -1,31 +1,44 @@
 import os
+from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_chroma import Chroma
-from langchain.vectorstores import Chroma
+from langchain_chroma import Chroma
+# from langchain.vectorstores import Chroma
 from langchain_community.document_loaders import CSVLoader
 from langchain_openai import OpenAIEmbeddings
+import pandas as pd
 
+load_dotenv()
 
-os.environ["OPENAI_API_KEY"] = 'SUA CHAVE DE API'
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
-pdf_path = 'carros.csv'
+# Modificado: definir o caminho absoluto para 'carros.csv'
+# pdf_path = "DATA-QUERY-AI/embeddings/ocorrencia.csv"
+pdf_path = "embeddings\ocorrencia.csv"
+
 loader = CSVLoader(pdf_path)
 docs = loader.load()
+print("docs: ", docs)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200,
-)
-chunks = text_splitter.split_documents(
-    documents=docs,
-)
+# # Se 'docs' for um DataFrame, converta-o em uma lista de strings
+# if isinstance(docs, pd.DataFrame):
+#     docs = docs.apply(lambda row: ' '.join(row.values.astype(str)), axis=1).tolist()
 
-persist_directory = 'db'
+# print("docs: ", docs)
 
-embedding = OpenAIEmbeddings()
-vector_store = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding,
-    persist_directory=persist_directory,
-    collection_name='carros',
-)
+# text_splitter = RecursiveCharacterTextSplitter(
+#     chunk_size=1000,
+#     chunk_overlap=200,
+# )
+# chunks = text_splitter.split_documents(
+#     documents=docs,
+# )
+
+# persist_directory = 'db'
+
+# embedding = OpenAIEmbeddings()
+# vector_store = Chroma.from_documents(
+#     documents=chunks,
+#     embedding=embedding,
+#     persist_directory=persist_directory,
+#     collection_name='ocorrencia',
+# )
--- embeddings/retriever.py
+++ embeddings/retriever.py
@@ -6,7 +6,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
 
-os.environ["OPENAI_API_KEY"] = 'SUA CHAVE DE API'
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 model = ChatOpenAI(
     model='gpt-4',
@@ -24,6 +24,13 @@ retriever = vector_store.as_retriever()
 
 system_prompt = '''
 Use o contexto para responder as perguntas.
+- Recupere os nomes das colunas e, em seguida, responda à pergunta com base nos dados.
+- Caso a pergunta esteja fora do contexto, responda: "Não posso responder este tipo de pergunta, pois foge do contexto passado."
+- **Antes de fornecer a resposta final**, utilize pelo menos um método adicional;
+reflita sobre ambos e verifique se os resultados respondem à pergunta original com precisão.
+- Formate números com quatro ou mais dígitos utilizando vírgulas.
+- Se os métodos divergem, reflita e tente outra abordagem; se ainda houver incerteza, reconheça.
+- A resposta final deve estar estruturada em markdown e sempre em Português Brasileiro.
 Contexto: {context}
 '''
 prompt = ChatPromptTemplate.from_messages(
